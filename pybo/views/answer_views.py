f3f4391110a5093b1e64a7baa90d323b852f4fbb
Taewoo-Lee <xodn3130@gmail.com>
2022-10-15 21:45:27 +0900

    질문 상세 페이지에서 글쓴이 관려 오류 수정

---
+++ pybo/views/answer_views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import redirect, get_object_or_404, resolve_url
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
+from ..models import Question, Answer
+from ..forms import AnswerForm
+
+
+@login_required(login_url='common:login')
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    return redirect('{}#answer_{}'.format(
+        resolve_url('pybo:detail', question_id=question.id), answer.id
+    ))
+
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    """
+    pybo 답변 수정
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정 권한이 없습니다.')
+        return redirect('pybo:detail', question_id=answer.question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return redirect(request, 'pybo/answer_form.html', context)
+
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    """
+    pybo 답변 삭제
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제 권한이 없습니다.')
+    else:
+        answer.delete()
+    return redirect('pybo:detail', question_id=answer.question.id)
