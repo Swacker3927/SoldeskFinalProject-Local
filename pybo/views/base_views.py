f888e61ada20e172766c0bb840b7a4a2b9686ad6
Taewoo-Lee <xodn3130@gmail.com>
2022-10-22 19:12:30 +0900

    로깅 관련 설정 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.db.models import Q
+import logging
+
 from ..models import Question
 
 
+logger = logging.getLogger('pybo')
+
 def index(request):
+    logger.info("INFO 레벨로 출력")
+
     # 입력 인자
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
